5b589b0871fc4c734244a66c8f10949928a5f2e6
jiratchayapu <70195642+jiratchayapu@users.noreply.github.com>
2022-01-23 20:25:59 +0700

    add log in page

---
+++ frontend/src/views/logIn/LogIn.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { Form, Input, Button, Typography, Row, message } from 'antd';
+import './LogIn.css';
+import * as api from '../../lib/api'
+const { Title } = Typography;
+const required = value => (value ? undefined : 'This field is required');
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined;
+
+class LogIn extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+              values: {},
+              error: {},
+            };
+            this.submit = this.submit.bind(this)
+            this.setFieldValue = this.setFieldValue.bind(this);
+            this.validators = {
+              email: [required, email],
+              password: [required],
+            };
+        }
+    async submit() {
+    if (this.validateAll()) {
+      const values = { ...this.state.values };
+      try {
+        const data = await api.user.login(values)
+        message.success('Registration Successful')
+        sessionStorage.setItem('token', JSON.stringify(data[0]._id));
+        window.location.href='/parties/'
+      } catch (error) {
+        message.error('Invalid email or password please try again')
+      }
+    }
+  }
+    setFieldValue(field, value) {
+      this.setState({
+        values: { ...this.state.values, [field]: value },
+        error: { ...this.state.error, [field]: null }
+      });
+    }
+    validateAll() {
+      const errors = {};
+      var valid = true;
+      //console.log(this.state.values)
+      Object.entries(this.validators).forEach(([field, validators]) => {
+        for (let i = 0; i < validators.length; ++i) {
+          const error = validators[i](this.state.values[field]);
+          if (error) {
+            errors[field] = error;
+            valid = false;
+            break;
+          }
+        }
+      });
+      this.setState({ error: { ...this.state.error, ...errors } });
+      return valid;
+    }
+  render() {
+    return (
+      <div className="mobile-background">
+      <div className="psr-login-card">
+        <Form colon={false}>
+          <Title style={{ textAlign: 'center', marginBottom: 16 }}>LOGO</Title>
+          <Title
+            level={2}
+            style={{ textAlign: 'center', marginTop: 0, marginBottom: 32 }}
+          >
+            login
+          </Title>
+          <Row             
+            justify="center"
+            >
+              <Form.Item
+               required
+              validateStatus={
+                  this.state.error.email ? 'error' : undefined
+                }
+                help={this.state.error.email}>
+                <Input placeholder="Email" 
+                value={this.state.values.email}
+                onChange={e => {
+                    this.setFieldValue('email', e.target.value);
+                  }}/>
+                </Form.Item>
+          </Row>
+          <Row             
+            justify="center"
+            >
+              <Form.Item
+              required
+              validateStatus={
+                  this.state.error.password ? 'error' : undefined
+                }
+                help={this.state.error.password}
+              >
+            <Input
+              type="password"
+              placeholder="Password"
+              value={this.state.values.password}
+              onChange={e => {
+                    this.setFieldValue('password', e.target.value);
+                  }}
+            />
+          </Form.Item>
+          </Row>
+          <Row justify="center">
+          <Form.Item>
+            <Button className='psr-primary-button' type="primary" htmlType="submit" shape="round" onClick={this.submit}>
+              Login
+            </Button>
+          </Form.Item>
+          </Row>
+          <Row justify="center">
+          <Form.Item className='register-button'>
+          <Button className='psr-primary-button' type="primary" htmlType="submit" shape="round" onClick={()=> window.location.href='/register'}>
+              Register
+            </Button>
+            </Form.Item>
+            </Row>
+        </Form>
+      </div>
+      </div>
+    );
+  }
+}
+
+
+export default LogIn;
